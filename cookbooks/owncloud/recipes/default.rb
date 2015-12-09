98882ad5f5862636d8903f3b1387038baa0b3d5a
Thomas Alrin <thomasalrin@megam.co.in>
2015-12-09 16:21:04 +0530

    owncloud public ip change

--- cookbooks/owncloud/recipes/default.rb
+++ cookbooks/owncloud/recipes/default.rb
@@ -33,7 +33,7 @@ def my_first_public_ipv4
   Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private?}
 end
 
-ip = my_first_private_ipv4.ip_address unless my_first_private_ipv4.nil?
+ip = my_first_public_ipv4.ip_address unless my_first_public_ipv4.nil?
 
 `sed -i "s/trusted_domains.*/trusted_domains' => array('#{ip}'),/" /var/www/owncloud/config/config.php`
 
