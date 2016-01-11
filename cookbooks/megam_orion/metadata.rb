4ff527ae929503a8e87fbd40e5fd290a7863dd68
vijaykanthm28 <mvijaykanth@megam.io>
2016-01-11 19:55:58 +0530

    removed unused directories and files

--- cookbooks/megam_orion/metadata.rb
+++ cookbooks/megam_orion/metadata.rb
@@ -1,7 +1,7 @@
 name             'megam_orion'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures megam_orion'
+maintainer       'megam systems'
+maintainer_email 'mvijaykanth@megam.io'
+license          'Apache 2.0'
+version          '0.5.0'
+description      'Installs/Configures a eclips orion app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
